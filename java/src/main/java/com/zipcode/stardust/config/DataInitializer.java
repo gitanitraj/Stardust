326729f95900ad831f25a841dc44cab74537a297
thejoeflynn <thejoeflynn@gmail.com>
2026-05-22 11:22:41 -0400

    Admin Tools added - default admin is "admin" with password "password"

--- java/src/main/java/com/zipcode/stardust/config/DataInitializer.java
+++ java/src/main/java/com/zipcode/stardust/config/DataInitializer.java
@@ -1,10 +1,13 @@
 package com.zipcode.stardust.config;
 
 import com.zipcode.stardust.model.Subforum;
+import com.zipcode.stardust.model.User;
 import com.zipcode.stardust.repository.SubforumRepository;
+import com.zipcode.stardust.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -13,8 +16,19 @@ public class DataInitializer implements ApplicationRunner {
     @Autowired
     private SubforumRepository subforumRepository;
 
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     @Override
     public void run(ApplicationArguments args) {
+        if (userRepository.findByUsername("admin").isEmpty()) {
+            User admin = new User("admin@localhost", "admin", "password", passwordEncoder);
+            userRepository.save(admin);
+        }
+
         if (subforumRepository.count() == 0) {
             Subforum forum = new Subforum("Forum",
                     "Announcements, bug reports, and general discussion about the forum belongs here", null);
